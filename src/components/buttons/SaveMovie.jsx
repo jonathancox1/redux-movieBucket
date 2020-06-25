f511783054fd5c72435682b7632aa22d658b822e
Jonathan Cox <cox.jonathan@gmail.com>
2020-06-25 15:28:57 -0400

    working app

--- src/components/buttons/SaveMovie.jsx
+++ src/components/buttons/SaveMovie.jsx
@@ -4,20 +4,14 @@ import { saveMovie } from '../../redux/actions/actions'
 
 export default function SaveMovie({ data }) {
     const dispatch = useDispatch();
-    const [string, setString] = useState('Save')
-
-    useEffect(() => {
-        //
-    }, [string])
 
     function saveItem() {
         dispatch(saveMovie(data))
-        setString('Saved')
     }
 
     return (
         <>
-            <button onClick={saveItem} className="text-center list-group-item list-group-item-action">{string}</button>
+            <button onClick={saveItem} className="text-center list-group-item list-group-item-action">Save</button>
         </>
     )
 }
